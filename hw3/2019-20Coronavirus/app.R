2da65d6bb0a7e27bc5bac55e5b858c1f3306a35a
xurui1996 <xurui1996@g.ucla.edu>
2020-02-29 06:38:19 +0000

    finish

--- hw3/2019-20Coronavirus/app.R
+++ hw3/2019-20Coronavirus/app.R
@@ -6,13 +6,12 @@ library(tidyverse)
 library(wesanderson)
 library(gapminder)
 library(gganimate)
-library(transformr)
 library(quantmod)
 
 # Load data ----
 ncov_tbl <- readRDS("ncov_tbl.rds")
 chn_prov <- readRDS("chn_prov.rds")
-
+countries <- readRDS("countries.rds")
 
 
 # Define tab-based UI
@@ -20,47 +19,38 @@ ui <- navbarPage(
     theme = shinytheme("united"),
     "2019-20 Coronavirus Outbreak",
     
-    #
+    # Plot a day in China
     tabPanel(
-        "A day",
-        titlePanel("Plot one case type on a specific date"),
+        "A day (CN)",
+        titlePanel("Plot one day's Coronavirus information in China"),
         sidebarLayout(
             sidebarPanel(
+                helpText("Plot one case type on a specific date vs province"),
                 dateInput("date1", "Select the date:", "2020-02-14"),
                 radioButtons("case1", "Select the case type:", c("confirmed", 
                              "recovered", "death"), selected = "confirmed")    
             ),
             mainPanel(
-                plotOutput(outputId = "plotaday"),
+                plotOutput(outputId = "plotaday")
+            )
+        ),
+        sidebarLayout(
+            sidebarPanel(
+                helpText("Plot three case types on a specific date 
+                         vs province"),
+                dateInput("date2", "Select the date:", "2020-02-14")   
+            ),
+            mainPanel(
                 plotOutput(outputId = "plotaday2")
             )
         )
     ),
     
-    #
+    # Plot over time in China
     tabPanel(
-        "Over time",
-        titlePanel("Plot the line graph of one case type over time"),
-        sidebarLayout(
-            sidebarPanel(
-                dateRangeInput("dates2",
-                               "Date range",
-                               start = "2020-01-21",
-                               end = as.character(Sys.Date())
-                ),
-                radioButtons("case2", "Select the case type:", c("confirmed", 
-                        "recovered", "death", "all"), selected = "confirmed")    
-            ),
-            mainPanel(
-                plotOutput(outputId = "plotvstime")
-            )
-        )
-    ),
-    
-    #
-    tabPanel(
-        "Animation",
-        titlePanel("Animation"),
+        "Over time (CN)",
+        titlePanel("Plot the line graph of one/all case type(s) over time 
+                   in China"),
         sidebarLayout(
             sidebarPanel(
                 dateRangeInput("dates3",
@@ -69,24 +59,76 @@ ui <- navbarPage(
                                end = as.character(Sys.Date())
                 ),
                 radioButtons("case3", "Select the case type:", c("confirmed", 
+                        "recovered", "death", "all"), selected = "confirmed")    
+            ),
+            mainPanel(
+                plotOutput(outputId = "plotvstime")
+            )
+        )
+    ),
+    
+    # Animation over time
+    tabPanel(
+        "Animation (CN)",
+        titlePanel("Animation over time in China"),
+        sidebarLayout(
+            sidebarPanel(
+                radioButtons("animation", "Select the animation:", 
+                             c("confirmed", "recovered", "death", "data"), 
+                             selected = "confirmed")    
+            ),
+            mainPanel(
+                imageOutput(outputId = "gif")
+            )
+        )
+    ),
+    
+    # Plot a day in the world
+    tabPanel(
+        "A day (national)",
+        titlePanel("Plot one case type on a specific date vs country"),
+        sidebarLayout(
+            sidebarPanel(
+                dateInput("date4", "Select the date:", "2020-02-14"),
+                radioButtons("case4", "Select the case type:", c("confirmed", 
                              "recovered", "death"), selected = "confirmed")    
             ),
             mainPanel(
-                imageOutput(outputId = "animation")
+                plotOutput(outputId = "plotadayn")
             )
         )
-        
     ),
     
-    #
+    # Plot over time in the world
+    tabPanel(
+        "Over time (national)",
+        titlePanel("Plot the line graph of one/all case type(s) over time 
+                   in the world"),
+        sidebarLayout(
+            sidebarPanel(
+                dateRangeInput("dates5",
+                               "Date range",
+                               start = "2020-01-21",
+                               end = as.character(Sys.Date())
+                ),
+                radioButtons("case5", "Select the case type:", c("confirmed", 
+                        "recovered", "death", "all"), selected = "confirmed")    
+            ),
+            mainPanel(
+                plotOutput(outputId = "plotvstimen")
+            )
+        )
+    ),    
+    
+    # Plot impact on economy
     tabPanel(
         "Stock",
-        titlePanel("Select a stock to examine.
+        titlePanel("Select a stock to examine. 
                  Information will be collected from yahoo finance"),
         sidebarLayout(
             sidebarPanel(
                 textInput("symb", "Symbol", "GOOG"),
-                dateRangeInput("dates4",
+                dateRangeInput("dates6",
                                "Date range",
                                start = "2020-01-21",
                                end = as.character(Sys.Date())
@@ -101,15 +143,6 @@ ui <- navbarPage(
             mainPanel(plotOutput(outputId = "plotstock"))
         )
     )
-    
-    # # previous outbreaks' information
-    # tabPanel(
-    #     titlePanel("Select a outbreak"), 
-    #     checkboxGroupInput("outbreak", "Select a outbreak:", c("2003 SARS", 
-    #                        "2009 H1N1", "2012 MERS", "2014 Ebola"), 
-    #                        selected = "2003 SARS")
-    # ),
-    # mainPanel(plotOutput("outbreak"))
 )
 
 # 
@@ -130,16 +163,18 @@ server <- function(input, output) {
             theme_bw() +
             labs(title = str_c(input$case1, " cases"), subtitle = input$date1)
     })
+    
+    #
     output$plotaday2 <- renderPlot({
         ncov_tbl %>%
             filter(`Country/Region` %in% c("Mainland China", "Macau",
                                            "Hong Kong", "Taiwan")) %>%
-            filter(Date == input$date1) %>%
+            filter(Date == input$date2) %>%
             ggplot() +
             geom_col(mapping = aes(x = `Province/State`, y = `Count`,
                                    fill = `Case`)) +
             scale_y_log10() +
-            labs(title = input$date1) +
+            labs(title = input$date2) +
             theme(axis.text.x = element_text(angle = 90))
     })
 
@@ -148,8 +183,8 @@ server <- function(input, output) {
         ncov_tbl %>%
             filter(`Country/Region` %in% c("Mainland China", "Macau",
                                            "Hong Kong", "Taiwan")) %>%
-            filter(Date >= input$dates2[1], Date <= input$dates2[2]) %>%
-            filter(if (input$case2 != "all") Case == input$case2
+            filter(Date >= input$dates3[1], Date <= input$dates3[2]) %>%
+            filter(if (input$case3 != "all") Case == input$case3
                    else Case == "confirmed" | Case == "recovered" | 
                         Case == "death") %>%
             group_by(Date, Case) %>%
@@ -164,33 +199,82 @@ server <- function(input, output) {
     })
 
     #
-    output$animation <- renderImage({
-        p <- ncov_tbl %>%
-            filter(`Country/Region` %in% c("Mainland China", "Macau",
+    output$gif <- renderImage({
+        # p <- str_c("../hw3sol/", input$animation, "_anim.gif") %>% 
+        #     as.character() %>% as.vector()
+        list(src = paste0("../hw3sol/", input$animation, "_anim.gif"))
+        # gif_file(p) %>%
+        #     print()
+    }, deleteFile = FALSE)
+    
+    #
+    output$plotadayn <- renderPlot({
+        info <- ncov_tbl %>%
+             filter(Date == input$date4, Case == input$case4) %>%
+             group_by(`Country/Region`) %>%  
+             top_n(1, Date)
+        China <- info %>%
+            filter(`Country/Region` %in% c("Mainland China", "Macau", 
                                            "Hong Kong", "Taiwan")) %>%
-            filter(Date >= input$dates3[1], Date <= input$dates3[2],
-                   Case == input$case3) %>%
-            right_join(chn_prov, by = c("Province/State" = "NAME_ENG")) %>%
-            ggplot() +
-            geom_sf(mapping = aes(fill = Count, geometry = geometry)) +
-            scale_fill_gradientn(colors = wes_palette("Zissou1", 100,
-                                 type = "continuous"), trans = "log10") +
+            group_by(Case) %>%
+            mutate(tmp = sum(Count)) %>% 
+            top_n(1, `Province/State`) %>%
+            select(-`Count`)
+        colnames(China)[7] <- "Count"
+        Other <- info %>% 
+            filter(`Country/Region` != "Mainland China", 
+                   `Country/Region` != "Macau", 
+                   `Country/Region` != "Hong Kong", 
+                   `Country/Region` != "Taiwan",
+                   `Country/Region` != "Others") %>%
+            mutate(tmp = sum(Count)) %>%
+            top_n(1, `Province/State`) %>%
+            select(- `Count`)
+        colnames(Other)[7] <- "Count"
+        newInfo <- bind_rows(China, Other)
+        map <- newInfo %>% 
+            left_join(countries, by = c("Country/Region" = "new_name"))
+        ggplot(map, aes(long, lat, group = group, color = Count)) + 
+            geom_polygon(aes(fill = Count)) + 
+            # scale_fill_gradientn(colours = wes_palette("Zissou1", 100, 
+            #                     type =   "continuous"), trans = "log10") + 
+            scale_fill_gradientn(
+                colours = c("#461863", "#404E88", "#2A8A8C", 
+                            "#7FD157", "#F9E53F"), 
+                values = scales::rescale(c(5, 10, 20, 100, 100000)), 
+                labels = scales::comma, 
+                breaks = c(5, 10, 20, 100, 100000)
+            ) +
+            guides(fill = guide_legend(reverse = T)) +
             theme_bw() +
-            labs(title = str_c(input$case3, " cases"))
-        anim <- p + 
-            transition_time(Date) + 
-            labs(title = str_c(input$case3, " cases"), 
-                 subtitle = "Date: {frame_time}")
-        anim_save("anim.gif", animate(anim, renderer = gifski_renderer()))
-        
+            labs(title = str_c(input$case4, " cases")) +
+            coord_quickmap()
+    })
+    
+    #
+    output$plotvstimen <- renderPlot({
+        ncov_tbl %>%
+            filter(Date >= input$dates5[1], Date <= input$dates5[2]) %>%
+            filter(if (input$case5 != "all") Case == input$case5
+                   else Case == "confirmed" | Case == "recovered" | 
+                       Case == "death") %>%
+            group_by(Date, Case) %>%
+            summarise(total_count = sum(Count)) %>%
+            ggplot() +
+            geom_line(mapping = aes(x = Date, y = total_count, color = Case),
+                      size = 2) +
+            scale_color_manual(values = c("blue", "black", "green")) +
+            scale_y_log10() +
+            labs(y = "Count") +
+            theme_bw()
     })
 
     #
     dataInput <- reactive({
         getSymbols(
             input$symb, src = "yahoo",
-            from = input$dates4[1],
-            to = input$dates4[2],
+            from = input$dates6[1],
+            to = input$dates6[2],
             auto.assign = FALSE
         )
     })
